4d908ddf347355703d9d715d568b9a092a8769d3
mrmooby <scotthayward@gmail.com>
2015-09-28 20:15:18 -0400

    Sound and hadouken added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // playtheme();
   $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
     $('.ryu-ready').show();
@@ -8,22 +9,30 @@ $(document).ready(function() {
     $('.ryu-still').show();
   })
   .mousedown(function() {
-    // play hadouken sound
+  	playhadouken();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    // animate hadouken to the right of the screen
-    $('.hadouken').show().animate(
+    $('.hadouken').finish().show().animate(
       {'left': '1020px'},
       500,
       function() {
         $(this).hide();
-        $(this).css('left', '520px');
+        $(this).css('left', '575px');
       }
   );
-    //end hadouken animation
   })
   .mouseup(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   });
 });
+function playhadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
+function playtheme() {
+    $('#ryu-theme')[0].volume = 0.1;
+    $('#ryu-theme')[0].load();
+    $('#ryu-theme')[0].play();
+}
