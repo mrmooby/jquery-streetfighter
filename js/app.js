d1a66f80cd410d514f358eab3e3ebf22d29f2fe2
mrmooby <scotthayward@gmail.com>
2015-09-29 16:14:43 -0400

    build complete

--- js/app.js
+++ js/app.js
@@ -44,10 +44,10 @@ function ryugo() {
 
   $(document).keydown(function(e) {
     if(e.keyCode == 88) {
-      $('.ryu-active').hide(); // I peeked at the HTML for the example project - when I saw that each div had multiple classes...  Ugh.
+      $('.ryu-active').hide();
       $('.ryu-cool').show();
     }
-  }).keyup(function(e) { // messed around with 2nd method code for 45 minutes before realizing that I missed the "e" in brackets...
+  }).keyup(function(e) { 
     if(e.keyCode == 88) {
       $('.ryu-cool').hide();
       $('.ryu-still').show();  
@@ -56,33 +56,3 @@ function ryugo() {
 }
 
 
-/*  MY ORIGINAL IF ATTEMPT - NOTE ELEMENTSTHAT I GOT RIGHT KEYCODE, ETC.
-
-  if ($('.ryu-still').is(':visible')) {
-// listen for x key
-    $('body').on('keydown', function(e){
-  	  if(e.keyCode == 88) {
-  	    $('.ryu-still').hide();
-  	    $('.ryu-cool').show();
-    }
-  })
-    $('body').on('keyup', function(){
- 	  $('.ryu-still').show();
-  	  $('.ryu-cool').hide();    
-    })
-  } else if ($('.ryu-still').is(':hidden')) {
-// listen for x key
-    $('body').on('keydown', function(e){
-  	  if(e.keyCode == 88) {
-  	    $('.ryu-ready').hide();
-  	    $('.ryu-cool').show();
-    }
-  })
-    $('body').on('keyup', function(){
- 	  $('.ryu-ready').show();
-  	  $('.ryu-cool').hide();    
-    })
-
-  }
-// end listen for x key
-*/
