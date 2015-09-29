3c3329c2ae723a9d44d203d0df0582658b863ee4
mrmooby <scotthayward@gmail.com>
2015-09-28 22:21:48 -0400

    Needs debugging and text

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  // playtheme();
+  playtheme();
   $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
     $('.ryu-ready').show();
@@ -9,7 +9,7 @@ $(document).ready(function() {
     $('.ryu-still').show();
   })
   .mousedown(function() {
-  	playhadouken();
+    playhadouken();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
     $('.hadouken').finish().show().animate(
@@ -19,20 +19,65 @@ $(document).ready(function() {
         $(this).hide();
         $(this).css('left', '575px');
       }
-  );
+    );
   })
   .mouseup(function() {
-    $('.ryu-throwing').hide();
-    $('.ryu-ready').show();
+    $('.ryu-active').hide();
+    $('.ryu-still').show();
+  });
+
+  $(document).keydown(function(e) {
+    if(e.keyCode == 88) {
+      $('.ryu-active').hide(); // I peeked at the HTML for the example project - when I saw that each div had multiple classes...  Ugh.
+      $('.ryu-cool').show();
+    }
+  }).keyup(function(e) { // messed around with 2nd method code for 45 minutes before realizing that I missed the "e" in brackets...
+    if(e.keyCode == 88) {
+      $('.ryu-cool').hide();
+      $('.ryu-active').show();  
+    }  
   });
 });
+
 function playhadouken() {
     $('#hadouken-sound')[0].volume = 0.5;
     $('#hadouken-sound')[0].load();
     $('#hadouken-sound')[0].play();
 }
+
 function playtheme() {
     $('#ryu-theme')[0].volume = 0.1;
     $('#ryu-theme')[0].load();
     $('#ryu-theme')[0].play();
 }
+
+/*  MY ORIGINAL IF ATTEMPT - NOTE ELEMENTSTHAT I GOT RIGHT KEYCODE, ETC.
+
+  if ($('.ryu-still').is(':visible')) {
+// listen for x key
+    $('body').on('keydown', function(e){
+  	  if(e.keyCode == 88) {
+  	    $('.ryu-still').hide();
+  	    $('.ryu-cool').show();
+    }
+  })
+    $('body').on('keyup', function(){
+ 	  $('.ryu-still').show();
+  	  $('.ryu-cool').hide();    
+    })
+  } else if ($('.ryu-still').is(':hidden')) {
+// listen for x key
+    $('body').on('keydown', function(e){
+  	  if(e.keyCode == 88) {
+  	    $('.ryu-ready').hide();
+  	    $('.ryu-cool').show();
+    }
+  })
+    $('body').on('keyup', function(){
+ 	  $('.ryu-ready').show();
+  	  $('.ryu-cool').hide();    
+    })
+
+  }
+// end listen for x key
+*/
